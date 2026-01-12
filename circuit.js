16dbc3e39f4a8e1b44eb5a09150d49503c82cb77
BERTAULT Nolhan <145433965+nbertault@users.noreply.github.com>
2026-01-12 09:31:27 +0100

    ajout video

--- circuit.js
+++ circuit.js
@@ -6,8 +6,6 @@ const description = document.getElementById('descCircuit');
 title.textContent = data[0].nomCircuit;
 description.textContent = data[0].descCircuit;
 
-document.getElementById('annee').textContent = new Date().getFullYear();
-
 carousel.addEventListener('slid.bs.carousel', function (event) {
     const index = event.to; // index du slide actif (Bootstrap 5)
     title.textContent = data[index].nomCircuit;
